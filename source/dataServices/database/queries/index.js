93e6f63c702e494fc797e58a2e1086eb6fc817c3
Stephan Weber <sdweber422@gmail.com>
2017-04-04 12:09:28 -0700

    Take user out of oauth file and make separate command and query with tests

--- source/dataServices/database/queries/index.js
+++ source/dataServices/database/queries/index.js
@@ -1,3 +1,4 @@
 export { getRecordById, findAllWhere, getUserByOAuthID } from './utilities'
 export { getProjectById, getProjectsByUserId } from './project'
 export { getCouldDoById, getCouldDosByProjectId } from './couldDo'
+export { getUserById } from './user'
